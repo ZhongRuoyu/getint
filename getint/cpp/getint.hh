b80da83de597bc5af782b25d746cd7a00c9a59d9
Zhong Ruoyu <zhongruoyu@outlook.com>
2020-05-22 01:53:40 +0800

    Files added

---
+++ getint/cpp/getint.hh
@@ -0,0 +1,40 @@
+/* Copyright (c) 2020 Zhong Ruoyu */
+
+#include <cstdio>
+
+int getint(int *errCode = NULL) {
+    if (!(errCode == NULL)) *errCode = 0; // Initialising errCode
+    int c = getchar(); // Temporarily stores next character from stdin
+    int sign = 1; // Sign of result: 1 = positive; -1 = negative
+    int absResult = 0; // Absolute value of result
+
+    while ((c == '\t') || (c == '\v') || (c == '\f') || (c == '\r') || (c == ' ')) c = getchar();
+    // Consumes all whitespaces at the front
+
+    while ((c == '-') || (c == '+')) {
+        if (c == '-') sign *= -1;
+        c = getchar();
+    } // Sign check
+
+    if ((c < '0') || (c > '9')) {
+        if (!(errCode == NULL)) *errCode = -1; // Invalid input: Not starting with a digit
+        return 0;
+    }
+
+    while ((c >= '0') && (c <= '9')) {
+        absResult = absResult * 10 + (c - '0');
+        c = getchar();
+    }
+    // Main
+
+    if (c == '\n') return (sign * absResult); // Success
+
+    do c = getchar(); while ((c == '\t') || (c == '\v') || (c == '\f') || (c == '\r') || (c == ' '));
+    // Consumes all whitespaces at the end
+
+    if (c == '\n') return (sign * absResult); // Success
+
+    fflush(stdin); // Clears stdin
+    if (!(errCode == NULL)) *errCode = -2; // Invalid input: Not ending with a digit
+    return 0;
+}
